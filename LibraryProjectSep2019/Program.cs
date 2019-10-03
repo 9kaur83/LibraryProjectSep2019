d7fe649010940a9d26ccd3d57a25a446a290484a
9kaur83 <55862564+9kaur83@users.noreply.github.com>
2019-10-03 03:09:15 -0700

    Initial commit

---
+++ LibraryProjectSep2019/Program.cs
@@ -0,0 +1,66 @@
+﻿using Stripe;
+using System;
+using Twilio.Types;
+
+namespace LibraryProjectSep2019
+{
+    class ProgramC
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer
+            {
+                CustomerName = "raj",
+                PhoneNumber = "98765437",
+                Email = "hjy@gmail.com",
+                Address = "34, DR SE bothell"
+            };
+
+            Console.WriteLine($"CN:{customer.CustomerName}" +
+                $",PN:{customer.PhoneNumber}" +
+                $",Email:{customer.Email}" +
+                $",address:{customer.Address}" +
+                $",UsID:{customer.UserIDOfCustomer}");
+
+            var customer2 = new Customer
+            {
+                CustomerName = "test",
+                PhoneNumber = "98765439",
+                Email = "abc@gmail.com",
+                Address = "Hwy st, Valley"
+            };
+
+            Console.WriteLine($"CN:{customer2.CustomerName}" +
+                $",PN:{customer2.PhoneNumber}" +
+                $",Email:{customer2.Email}" +
+                $",address:{customer2.Address}" +
+                $",UsID:{customer2.UserIDOfCustomer}");
+
+            var book = new Book
+            {
+                BookName = "abc",
+                IsbnNumber = "9865558"
+            };
+      
+            Console.WriteLine($"BN:{book.BookName}" +
+                $",IN:{book.IsbnNumber}" +
+                $",Issued:{book.IssuedUserID != 0}");
+
+            book.IssueBook(customer.UserIDOfCustomer);
+
+            Console.WriteLine($"BN:{book.BookName}" +
+                $",IN:{book.IsbnNumber}" +
+                $",Issued:{book.IssuedUserID != 0}" +
+                $",IssuedDate:{book.IssueDate}" +
+                $",IssuedByUserId:{book.IssuedUserID}");
+
+            book.ReturnBook();
+
+            Console.WriteLine($"BN:{book.BookName}" +
+                $",IN:{book.IsbnNumber}" +
+                $",Issued:{book.IssuedUserID != 0}" +
+                $",IssuedDate:{book.IssueDate}");
+        }
+    }
+}
+
---
+++ LibraryProjectSep2019/books.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryProjectSep2019
+{
+    class Book
+    {
+        #region Properties
+        /// <summary>
+        /// Name of the Book.
+        /// </summary>
+        public string BookName { get; set; }
+        /// <summary>
+        /// ISBN number of the Book.
+        /// </summary>
+        public string IsbnNumber { get; set; }
+        /// <summary>
+        /// User ID of the customer who issued the Book. 0 indicates  Book not issued.
+        /// </summary>
+        public int IssuedUserID { get; set; }
+        /// <summary>
+        /// Date and time book issued.
+        /// </summary>
+        public DateTime IssueDate { get; private set; }
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Constructor for Book class
+        /// </summary>
+        public Book()
+        {
+            IssuedUserID = 0;
+            IssueDate = DateTime.MaxValue;
+        }
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Method to Issue Book to particular user from Library.
+        /// </summary>
+        /// <param name="userId">User ID of customer that wants to issue the Book</param>
+        public void IssueBook(int userId)
+        {
+            IssuedUserID = userId;
+            IssueDate = DateTime.Now;
+        }
+
+        /// <summary>
+        /// Method to return Book back to Library
+        /// </summary>
+        public void ReturnBook()
+        {
+            IssuedUserID = 0;
+            IssueDate = DateTime.MaxValue;
+        }
+
+        #endregion
+    }
+}
